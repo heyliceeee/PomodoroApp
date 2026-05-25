47d30abf96e414f3270cf4735e40a70acff440da
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-05-25 12:29:14 +0100

    💄feat: create the buttons

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ dir_path = os.path.dirname(os.path.abspath(__file__)) # get the path of the curr
 
 window = Tk() # create a window
 
+
+def timer_start():
+    """
+    timer start
+    :return:
+    """
+    pass
+def timer_reset():
+    """
+    timer reset
+    :return:
+    """
+    pass
+
 # UI setup
 def create_window():
     """
@@ -21,15 +35,14 @@ def create_window():
     """
     window.title("Pomodoro") # set the title of the window
     window.config(padx=100, pady=50) # set the padding of the window\
-    window.config(bg=YELLOW) # set the background color of the window
+    window.tk.call("tk_setPalette", YELLOW) # set the background color of the window
 def create_canvas():
     """
     create a canvas
     """
-    canvas = Canvas(width=200, height=224) # create a canvas
-    canvas.config(bg=YELLOW, highlightthickness=0) # set the background color of the canvas
+    tomato_img = PhotoImage(file=dir_path + IMAGE)  # create an image
 
-    tomato_img = PhotoImage(file=dir_path + IMAGE) # create an image
+    canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) # create a canvas
     canvas.tomato_img = tomato_img # assign the image to a variable
     canvas.create_image(100, 112, image=tomato_img) # place the image
     canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
@@ -38,17 +51,23 @@ def create_labels():
     """
     create the labels, set the text, and place them on the window
     """
-    timer_title_label = Label(text="Timer", font=(FONT_NAME, 60)) # create the label
+    timer_title_label = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW) # create the label
     timer_title_label.grid(column=1, row=0) # place the label on the window
-    timer_title_label.config(fg=GREEN) # set the color of the label
-    timer_title_label.config(bg=YELLOW) # set the background color of the label
 
-    check_label = Label(text="✓", font=(FONT_NAME, 30)) # create the label
+    check_label = Label(text="✓", font=(FONT_NAME, 30), fg=GREEN, bg=YELLOW) # create the label
     check_label.grid(column=1, row=3) # place the label on the window
-    check_label.config(fg=GREEN) # set the color of the label
-    check_label.config(bg=YELLOW) # set the background color of the label
+def create_btns():
+    """
+    create the buttons, set the text, and place them on the window
+    """
+    start_btn = Button(text="Start", command=timer_start, bg=YELLOW, highlightthickness=0) # create the button
+    start_btn.grid(column=0, row=2) # place the button on the window
 
+    reset_btn = Button(text="Reset", command=timer_reset, bg=YELLOW, highlightthickness=0)  # create the button
+    reset_btn.grid(column=2, row=2) # place the button on the window
+    
 create_window() # call the function to create the window
 create_canvas() # call the function to create the canvas
 create_labels() # call the function to create the labels
+create_btns() # call the function to create the buttons
 window.mainloop() # continuously run the program
